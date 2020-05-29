ce9bfc392d532768dd6c53cf9e36c3f12bf1afeb
Shun <shunkazami9991@gmail.com>
2020-05-30 01:04:46 +0530

    fetch from server (json-sever)

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent'
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -78,7 +79,7 @@ function RenderDish({dish}) {
     return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle heading="true"><strong>{dish.name}</strong></CardTitle>
                     <CardText>{dish.description}</CardText>
