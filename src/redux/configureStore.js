c00237fb4f9c584e0995570d6e65de6e7412f8f4
Shun <shunkazami9991@gmail.com>
2020-05-29 15:25:21 +0530

    react-redux-form(rrf) revisited added actions provided by rrf to the form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createForms} from 'react-redux-form';
 import {Dishes} from './dishes.js';
 import {Leaders} from './leaders.js';
 import {Promotions} from './promotions.js';
 import {Comments} from './comments.js';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-
+import {InitialFeedback} from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -13,7 +14,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
